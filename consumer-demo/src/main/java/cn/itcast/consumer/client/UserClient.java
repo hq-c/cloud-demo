66522176510a0f8fec084812ee58aef997677fee
BD0B0987332A7119A7A1DCF21A71C569 <BD0B0987332A7119A7A1DCF21A71C569@i-search.com.cn>
2020-04-22 22:13:15 +0800

    6666

--- consumer-demo/src/main/java/cn/itcast/consumer/client/UserClient.java
+++ consumer-demo/src/main/java/cn/itcast/consumer/client/UserClient.java
@@ -3,13 +3,15 @@ package cn.itcast.consumer.client;
 import cn.itcast.consumer.pojo.User;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
-/*请求服务名*/
+/*请求服务名-0422*/
 @FeignClient(value = "user-server",fallback = UserClientFallback.class)
 public interface UserClient {
     /*请求方式，请求路径，返回数据类型，请求参数*/
     @GetMapping("user/{id}")
     String getUserById(@PathVariable("id") Long id);
+    @RequestMapping(value = "/save",method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    String saveUser(User user);
+
 }
